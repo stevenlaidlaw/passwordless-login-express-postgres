8478157f98d08bdd43581e9ca455a85f16398065
Steven Laidlaw <steven@stevenlaidlaw.com>
2022-03-31 15:49:41 +0800

    Thu 31 Mar 2022 15:49:41 AWST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const home = require("./home.js");
 const login = require("./login.js");
 const register = require("./register.js");
+const account = require("./account.js");
 
 module.exports = (app) => {
   app.use("/", home);
   app.use("/login", login);
   app.use("/register", register);
+  app.use("/account", account);
 };
